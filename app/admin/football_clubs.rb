c0dc7593a4a060644aa6b36acd4bac6e9114f5a8
Almin Karic <almin_k@live.com>
2018-05-20 00:38:34 +0200

    added parent to active admin menus

--- app/admin/football_clubs.rb
+++ app/admin/football_clubs.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Football::Club do
+  menu parent: 'Football', label: 'Clubs'
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
